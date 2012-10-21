46eb59e4cf19eb863cb1fd59c74f8e9efb0513e2
NewbyJE <NewbyJE@comcast.net>
2012-10-21 07:14:07 -0700

    README: Update to current build

--- patches/4G_jb_alpha.sh
+++ patches/4G_jb_alpha.sh
@@ -17,22 +17,31 @@ repo sync -j16
 #
 echo "apply device/hp/tenderloin cherrypicks"
 pushd device/hp/tenderloin
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/15/24215/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/15/24215/1 && git cherry-pick -n FETCH_HEAD
+git reset HEAD
 popd
 #
 echo "apply frameworks/base cherrypicks"
 pushd frameworks/base
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/83/24883/1 && git cherry-pick FETCH_HEAD
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/57/24957/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/83/24883/1 && git cherry-pick -n FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/57/24957/1 && git cherry-pick -n FETCH_HEAD
+git reset HEAD
 popd
 #
 echo "apply kernel/hp/tenderloin cherrypicks"
 pushd kernel/hp/tenderloin
-git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/16/24816/1 && git cherry-pick FETCH_HEAD
-git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/17/24817/1 && git cherry-pick FETCH_HEAD
-git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/18/24818/1 && git cherry-pick FETCH_HEAD
-git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/19/24819/1 && git cherry-pick FETCH_HEAD
-git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/49/24149/2 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/16/24816/1 && git cherry-pick -n FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/17/24817/1 && git cherry-pick -n FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/18/24818/1 && git cherry-pick -n FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/19/24819/1 && git cherry-pick -n FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/hp-kernel-tenderloin refs/changes/49/24149/2 && git cherry-pick -n FETCH_HEAD
+git reset HEAD
+popd
+#
+echo "special, revert qcom update until fixed"
+pushd hardware/qcom/display
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_display refs/changes/74/24874/1 && git cherry-pick -n FETCH_HEAD
+git reset HEAD
 popd
 #
 echo "development patch"
@@ -48,6 +57,7 @@ cd ~/android/cm10jb
 echo "kernel_hp_tenderloin patch"
 cd kernel/hp/tenderloin
 git apply ~/android/cm10jb/vendor/mbm/patches/kernel_hp_tenderloin.patch
+git apply ~/android/cm10jb/vendor/mbm/patches/kernel_hp_tenderloin_A6.patch
 cd ~/android/cm10jb
 #
 echo "system_core patch"
